cdadc3f4b53a84a2e6f8faabe83c75153d3aa4eb
kushtriimhyseni <kushtrim@starlabspro.com>
2023-04-18 03:48:28 +0200

    Added styles on routes

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,7 @@
-export default function page() {
-  return <div>Project Page</div>;
+export default function ProjectPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center max-w-screen-lg mx-auto p-4">
+      <div className="w-full pt-24">Project Page</div>
+    </main>
+  );
 }
