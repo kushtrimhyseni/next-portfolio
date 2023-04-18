d09d6b96c712476c0a9436246d9197d146e3aa13
kushtriimhyseni <kushtrim@starlabspro.com>
2023-04-18 03:37:48 +0200

    Update ProjectCard.tsx

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -5,7 +5,7 @@ interface Projects {
   id: number;
   project_name: string;
   project_image: string;
-  project_title: string;
+  project_button: string;
   project_description: string;
   project_technology: string;
 }
