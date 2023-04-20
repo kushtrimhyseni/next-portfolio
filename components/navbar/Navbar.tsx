f5521f87c09ef33247ee29b351120a56dd5e0e79
kushtriimhyseni <kushtrim@starlabspro.com>
2023-04-20 03:37:17 +0200

    Added link to homepage for logos

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import NavBarLinks from "./NavBarLinks";
 import logo from "../../public/images/logo_1.svg";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div className="flex justify-between items-center max-w-screen-lg mx-auto mt-8 px-2">
-      <Image
-        src={logo}
-        alt="Logo"
-        width={48}
-        height={48}
-        className="w-12 h-12"
-      />
+      <Link href="/">
+        <Image
+          src={logo}
+          alt="Logo"
+          width={48}
+          height={48}
+          className="w-12 h-12"
+        />
+      </Link>
       <NavBarLinks />
     </div>
   );
